a6cd2c9a782563cd35d24ca587b1b732e22ae1d3
Applelil <146865243+Applelil@users.noreply.github.com>
2023-12-27 22:34:16 -0800

    Added custom transformer classes to library.py

--- library.py
+++ library.py
@@ -4,3 +4,114 @@ import sklearn
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+#
+#Transformer Classes
+#
+
+#Custom Mapping Transformer
+class CustomMappingTransformer(BaseEstimator, TransformerMixin):
+
+  def __init__(self, mapping_column, mapping_dict:dict):
+    assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.'
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.mapping_column}"'  #column legit?
+
+    #Set up for producing warnings. First have to rework nan values to allow set operations to work.
+    #In particular, the conversion of a column to a Series, e.g., X[self.mapping_column], transforms nan values in strange ways that screw up set differencing.
+    #Strategy is to convert empty values to a string then the string back to np.nan
+    placeholder = "NaN"
+    column_values = X[self.mapping_column].fillna(placeholder).tolist()  #convert all nan values to the string "NaN" in new list
+    column_values = [np.nan if v == placeholder else v for v in column_values]  #now convert back to np.nan
+    keys_values = self.mapping_dict.keys()
+
+    column_set = set(column_values)  #without the conversion above, the set will fail to have np.nan values where they should be.
+    keys_set = set(keys_values)      #this will have np.nan values where they should be so no conversion necessary.
+
+    #now check to see if all keys are contained in column.
+    keys_not_found = keys_set - column_set
+    if keys_not_found:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] these mapping keys do not appear in the column: {keys_not_found}\n")
+
+    #now check to see if some keys are absent
+    keys_absent = column_set -  keys_set
+    if keys_absent:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] these values in the column do not contain corresponding mapping keys: {keys_absent}\n")
+
+    #do actual mapping
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    #self.fit(X,y)
+    result = self.transform(X)
+    return result
+
+#Custom Renaming Transformer
+class CustomRenamingTransformer(BaseEstimator, TransformerMixin):
+
+  #your __init__ method below
+  def __init__(self, col_transform):
+    assert isinstance(col_transform, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(col_transform)} instead.'
+    self.col_transform = col_transform
+
+  #define fit to do nothing but give warning
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return self
+
+  #write the transform method with asserts. Again, maybe copy and paste from MappingTransformer and fix up.
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    all_col_names = X.columns.to_list()
+    old_col = self.col_transform.keys()
+    all_col_names_set = set(all_col_names)
+    old_col_set = set(old_col)
+    common_col = all_col_names_set & old_col_set
+    wrong_col = old_col_set - common_col
+    assert not wrong_col, f"\nWarning: {self.__class__.__name__}[{self.col_transform}] column(s) unknown: {wrong_col}\n"
+
+    #do actual renaming
+    X_ = X.copy()
+    X_.rename(columns = self.col_transform, inplace = True)
+    return X_
+
+  #write fit_transform that skips fit
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+#Custome OHE Transformer
+class CustomOHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=False):
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+
+  #fill in the rest below
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return self
+
+  def transform(self, X):
+    assert self.target_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.target_column}"'  #column legit?
+
+    #do actual OHE
+    X_ = X.copy()
+    X_ohe = pd.get_dummies(X_, prefix = self.target_column, prefix_sep = '_', columns = [self.target_column], dummy_na = self.dummy_na, drop_first = self.drop_first)
+
+    return X_ohe
+
+  def fit_transform(self, X, y = None):
+    #self.fit(X,y)
+    result = self.transform(X)
+    return result
